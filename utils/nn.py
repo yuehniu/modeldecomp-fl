8e5a7b7a42e41463f8483820e28ced4a83ad6fcf
Julien <30472512+yuehniu@users.noreply.github.com>
2022-01-31 12:11:34 -0800

    ADD: vgg model definition.

--- utils/nn.py
+++ utils/nn.py
@@ -2,9 +2,23 @@
 
 Note:
 """
+from model.vgg import vgg11, vgg11_bn, vgg16, vgg16_bn, vgg19, vgg19_bn
+from model.resnetcifar import resnet18, resnet34
 
-def build_network():
-    pass
+def build_network( model_name, **kwargs ):
+    models = {
+        'vgg11': vgg11,
+        'vgg11_bn': vgg11_bn,
+        'vgg16': vgg16,
+        'vgg16_bn': vgg16_bn,
+        'vgg19': vgg19,
+        'vgg19_bn': vgg19_bn,
+        'resnet18': resnet18,
+        'resnet34': resnet34
+    }
+
+    return models[ model_name ]( **kwargs )
+
 
 
 def convert_to_principal_model:
