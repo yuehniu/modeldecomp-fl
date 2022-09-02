2d39911b4695c7f00a00f9c3c51a806c23e7df84
YUE NIU <yueniu@usc.edu>
2022-09-01 17:22:35 -0700

    MODIFY: weight decay on sub low-rank layers.

--- utils/frob.py
+++ utils/frob.py
@@ -21,24 +21,27 @@ def add_frob_decay( model, alpha=0.0001 ):
                     __recursive_add_decay( m )
                 elif isinstance( m, Conv2d_Orth ):
                     # compute Frobenius decay
-                    ichnls, ochnls = m.conv2d_V.in_channels, m.conv2d_V.out_channels
+                    v_ichnls, v_ochnls = m.conv2d_V.in_channels, m.conv2d_V.out_channels
+                    u_ichnls, u_ochnls = m.conv2d_U.in_channels, m.conv2d_U.out_channels
                     sz_kern = m.conv2d_V.kernel_size
-                    m_V = m.conv2d_V.weight.data.view(ochnls, ichnls * sz_kern[0] * sz_kern[1])
+                    m_V = m.conv2d_V.weight.data.view( v_ochnls, v_ichnls * sz_kern[0] * sz_kern[1] )
                     m_V *= m.chnl_mask.unsqueeze( 1 )  # only perform decay non-masked channels
-                    m_s = m.conv2d_S.weight.data.view(ochnls, )
-                    m_U = m.conv2d_U.weight.data.view(ochnls, ochnls)
-                    m_U *= m.chnl_mask.unsqueeze( 0 )  # only perform decay non-masked channels
-                    # TODO: mask U output channel
-                    # m_U *= m.chnl_mask.unsqueeze( 0 ).T
+                    m_s = m.conv2d_S.weight.data.view( v_ochnls, )
+                    m_U = m.conv2d_U.weight.data.view( u_ochnls, u_ichnls )
+                    # m_U *= m.chnl_mask.unsqueeze( 0 )  # only perform decay non-masked channels
+                    m_U *= m.chnl_mask_u  # mask both input and output channels
 
                     US = torch.matmul( m_U, torch.diag( m_s ) )
                     SV = torch.matmul( torch.diag( m_s ), m_V )
+                    # US = torch.matmul( m_U, torch.diag( torch.sqrt( m_s ) ) )
+                    # SV = torch.matmul( torch.diag( torch.sqrt( m_s ) ), m_V )
+                    # US, SV = m_U, m_V
 
                     U_frob = torch.linalg.multi_dot( ( m_U, SV, SV.T ) )
                     V_frob = torch.linalg.multi_dot( ( US.T, US, m_V ) )
 
-                    U_grad = U_frob.view( ochnls, ochnls, 1, 1 )
-                    V_grad = V_frob.view( ochnls, ichnls, *sz_kern )
+                    U_grad = U_frob.view( u_ochnls, u_ichnls, 1, 1 )
+                    V_grad = V_frob.view( v_ochnls, v_ichnls, *sz_kern )
 
                     m.conv2d_U.weight.grad += alpha * U_grad
                     m.conv2d_V.weight.grad += alpha * V_grad
